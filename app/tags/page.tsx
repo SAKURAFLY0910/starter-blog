d2fd7d54920a6b6e1d7f87c90feb9ee27605932a
SAKURAFLY0910 <sakurafly.com@gmail.com>
2026-01-03 19:36:55 +0800

    slugify

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
               <div key={t} className="mt-2 mr-5 mb-2">
                 <Tag text={t} />
                 <Link
-                  href={`/tags/${encodeURIComponent(t)}`}
+                  href={`/tags/${encodeURIComponent(slugify(t))}`}
                   className="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300"
                   aria-label={`View posts tagged ${t}`}
                 >
