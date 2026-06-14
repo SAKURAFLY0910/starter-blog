cde3a267c7af82fdfdd37ded3e412f0c3aa7c3b9
SAKURAFLY0910 <sakurafly.com@gmail.com>
2026-06-14 20:40:02 +0800

    20260614
    1. 去除文章大间隔
    2. 修改描述

--- app/Main.tsx
+++ app/Main.tsx
@@ -15,8 +15,7 @@ export default function Home({ posts }) {
           {/* <h1 className="font-serif text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">时光随笔</h1> */}
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {/* {siteMetadata.description} */}
-            此生今已惯 再会永无期 唯有心头恋 缠绵到死时 <br />
-            万物难为有 无常似尾花 空蝉如此世 幻灭若朝霞
+            万物难为有 无常似尾花 空蝉如此世 幻灭若朝霞 
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -24,7 +23,7 @@ export default function Home({ posts }) {
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags, images: images = [] } = post
             return (
-              <li key={slug} className="py-12">
+              <li key={slug} className="py-6">
                 <article>
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                     <dl>
@@ -43,7 +42,7 @@ export default function Home({ posts }) {
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
-                          <h2 className="text-2xl leading-8 font-bold tracking-tight">
+                          <h2 className="text-xl leading-8 font-bold tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
